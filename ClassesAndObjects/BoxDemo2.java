c79ebd1bceab38c541f6c68c72af08ff832b4cf6
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-03-26 06:13:53 +0000

    compare the two boxes whether it is same or not

--- ClassesAndObjects/BoxDemo2.java
+++ ClassesAndObjects/BoxDemo2.java
@@ -7,5 +7,18 @@ public class BoxDemo2
         B1.showBox("B1");
         System.out.println("Volume of the Box B1 is : " + B1.volume());
         System.out.println();
+
+        Box B2 = new Box();
+        B2.setBox(11.11, 22.22, 33.33);
+        B2.showBox("B2");
+        System.out.println("Volume of the Box B2 is : " + B2.volume());
+
+        //we can create multiple object to perform the operation
+        
+        //comparing the two object whether it is same or not 
+        if(B2.compare(B1))
+            System.out.println("B2 == B1");
+        else 
+            System.out.println("B != B2");
     }
 }
