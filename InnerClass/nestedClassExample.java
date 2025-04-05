740deb5700685355c435276e212cf580e4b71de0
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-05 18:46:01 +0000

    example of outer class which is using the properties and methods of inner class.

---
+++ InnerClass/nestedClassExample.java
@@ -0,0 +1,24 @@
+public class nestedClassExample {
+    private String message = "Hi from innerClassExample";
+    class innerClassExample
+    {
+        innerClassExample()
+        {
+            System.out.println(message);
+        }
+        void show()
+        {
+            System.out.println("Hello hii from show function of innerClassExample.");
+        }
+    }
+    void display()
+    {
+        innerClassExample obj = new innerClassExample();
+        obj.show();
+    }
+    public static void main(String[] args) {
+        nestedClassExample outer = new nestedClassExample();
+        outer.display();
+        nestedClassExample.innerClassExample inner = outer.new innerClassExample();
+    }
+}
