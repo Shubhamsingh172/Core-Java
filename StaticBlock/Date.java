98a336832d6b16ac7bd0b6007c683eca30a32cd8
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-06 19:26:29 +0000

    Date program which is nothing but a simple program by using which we can store the date in date data type which is user defined

---
+++ StaticBlock/Date.java
@@ -0,0 +1,27 @@
+package StaticBlock;
+
+public class Date {
+    int day;
+    int month;
+    int year;
+    Date()
+    {
+        day = month = year;
+    }
+    Date(int dd, int mm, int yy)
+    {
+        day = dd;
+        month = mm;
+        year = yy;
+    }
+    void setDate(int dd, int mm, int yy)
+    {
+        day = dd;
+        month = mm;
+        year = yy;
+    }
+    void showDate()
+    {
+        System.out.printf("%02d/%02d/%04d\n",day,month, year);
+    }
+}
---
+++ StaticBlock/DateDemo.java
@@ -0,0 +1,11 @@
+package StaticBlock;
+
+public class DateDemo {
+    public static void main(String[] args)
+    {
+        Date d1 = new Date();
+        d1.showDate();
+        Date d2 = new Date(17, 3, 2004);
+        d2.showDate();
+    }
+}
