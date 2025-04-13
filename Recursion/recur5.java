38c9ab89149df851d6844cabf1c0f2ac3345bb68
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-13 13:52:06 +0000

    recurison program no. 5

---
+++ Recursion/recur5.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class recur5 {
+    static int sumofNNatural(int n)
+    {
+        if(n <= 1)
+            return 1;
+        return n + sumofNNatural(n - 1);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        System.out.println("Sum of first " +  n + " natural number is : " + sumofNNatural(n));
+    }
+}
