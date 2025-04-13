901643f6392b7b2079fa7907c891699579e0e162
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-13 13:31:57 +0000

    recursison program no. 2

---
+++ Recursion/recur2.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class recur2 {
+    static void fun(int n)
+    {
+        if(n > 100)
+            return;
+        System.out.println("India");
+    }
+    public static void main(String[] args) {
+        fun(99);
+    }
+}
