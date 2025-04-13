423c71b071176c63ddc3acdb013a4d8197f505f4
Shubham Shashibhushan Singh <146461779+Shubhamsingh172@users.noreply.github.com>
2025-04-13 13:59:01 +0000

    recursion 6 program

---
+++ Recursion/recur6.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class recur6 {
+    static int fact(int n) {
+        if (n <= 1)
+            return 1;
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n = sc.nextInt();
+        System.out.println("Factorial of number " + n + " is : " + fact(n));
+    }
+}
